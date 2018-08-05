c3ee70f5fb2a1511ed74d9ad1ca06724e31e8c6f
ipman98 <navilk@zoho.com>
2018-08-05 15:54:25 +0530

    change: data tables from cdn

--- items.php
+++ items.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Easy System : Stock Items</title>
   <link rel="stylesheet" href="./res/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="./res/DataTables/datatables.min.css"/>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.2/b-flash-1.5.2/b-html5-1.5.2/b-print-1.5.2/r-2.2.2/datatables.min.css"/>
 </head>
 <body>
   <?php require_once './res/content/navBar.php'; ?>
@@ -184,7 +184,9 @@
   <!-- scripts -->
   <script src="./res/js/jquery-3.3.1.min.js" charset="utf-8"></script>
   <script src="./res/js/bootstrap.min.js" charset="utf-8"></script>
-  <script type="text/javascript" src="./res/DataTables/datatables.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.2/b-flash-1.5.2/b-html5-1.5.2/b-print-1.5.2/r-2.2.2/datatables.min.js"></script>
   <script src="./res/js/items.js" charset="utf-8"></script>
 </body>
 </body>
